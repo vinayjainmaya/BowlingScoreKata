12c9a25a0280c41021a41f25b7268f6eed02d91d
Vinay Jain <vinayjainmaya@gmail.com>
2023-02-03 21:00:14 +0530

    Refactor: improved spare condition

--- app/src/main/java/com/vinaymj/bowlingscore/domain/FrameScores.kt
+++ app/src/main/java/com/vinaymj/bowlingscore/domain/FrameScores.kt
@@ -5,6 +5,6 @@ data class FrameScores(
     val second: Int,
     var total: Int = 0
 ) {
-    val spare = (first + second) == 10
+    val spare = ((first != 10 && second != 10) && (first + second) == 10)
     val strike = (first == 10 || second == 10)
 }
