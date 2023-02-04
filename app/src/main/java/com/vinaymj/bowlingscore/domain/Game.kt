49b5abea607c967b02ceec6de39240a0b95e7b8e
Vinay Jain <vinayjainmaya@gmail.com>
2023-02-04 18:23:59 +0530

    Refactor: Created separated methods

--- app/src/main/java/com/vinaymj/bowlingscore/domain/Game.kt
+++ app/src/main/java/com/vinaymj/bowlingscore/domain/Game.kt
@@ -12,7 +12,7 @@ class Game {
         calculateBonus(frameScore)
 
         val currentScore = frameScores[frameScores.size]?.gameTotal ?: 0
-        frameScore.gameTotal = if(frameScores.size == 9){
+        frameScore.gameTotal = if (frameScores.size == 9) {
             frame.score(frameScore.frameTotal, frameScore.third, currentScore)
         } else {
             frame.score(frameScore.first, frameScore.second, currentScore)
@@ -23,59 +23,92 @@ class Game {
 
 
     private fun calculateBonus(currentFrame: FrameScores) {
-        when(frameScores.size) {
-            0 -> {return}
-            1 -> {
-                val lastFrame = frameScores[frameScores.size]
-                if (lastFrame != null) {
-                    lastFrame.gameTotal = when {
-                        (lastFrame.strike && !currentFrame.strike) -> {
-                            frame.scoreWithBonus(lastFrame.gameTotal, currentFrame.frameTotal)
-                        }
-                        (lastFrame.spare) -> {
-                            frame.scoreWithBonus(lastFrame.gameTotal,currentFrame.first)
-                        }
-                        else -> {lastFrame.gameTotal}
-                    }
-                }
-            }
+        when (frameScores.size) {
+            0 -> return
+            1 -> firstFrameBonus(currentFrame)
             else -> {
                 val lastFrame = frameScores[frameScores.size]
                 val secondLastFrame = frameScores[frameScores.size - 1]
-                if(lastFrame != null && secondLastFrame != null) {
-                    secondLastFrame.gameTotal = when {
-                        (secondLastFrame.strike && lastFrame.strike) -> {
-                            frame
-                                .scoreWithBonus(secondLastFrame.gameTotal,(lastFrame.first + currentFrame.first))
-                        }
-                        else -> {secondLastFrame.gameTotal}
-                    }
-                    lastFrame.gameTotal = when {
-                        (frameScores.size == 9) -> {
-                            when {
-                                (lastFrame.strike) -> {
-                                    frame.scoreWithBonus(secondLastFrame.gameTotal, (lastFrame.first + currentFrame.frameTotal))
-                                }
-                                (lastFrame.spare) -> {
-                                    frame.scoreWithBonus(secondLastFrame.gameTotal, (lastFrame.frameTotal + currentFrame.first))
-                                }
-                                else -> {
-                                    frame.score(lastFrame.first ,lastFrame.second, secondLastFrame.gameTotal)
-                                }
-                            }
-                        }
-                        (lastFrame.strike && !currentFrame.strike) -> {
-                            frame.scoreWithBonus(secondLastFrame.gameTotal, (lastFrame.first + currentFrame.frameTotal))
-                        }
-                        (lastFrame.spare) -> {
-                            frame.scoreWithBonus(secondLastFrame.gameTotal,(lastFrame.frameTotal + currentFrame.first))
-                        }
-                        else -> {
-                            frame.score(lastFrame.first, lastFrame.second, secondLastFrame.gameTotal)
-                        }
-                    }
+                if (lastFrame != null && secondLastFrame != null) {
+                    secondLastFrame.gameTotal =
+                        multiStrikeSequenceBonus(secondLastFrame, lastFrame, currentFrame)
+                    lastFrame.gameTotal =
+                        previousFrameBonus(lastFrame, secondLastFrame, currentFrame)
                 }
             }
+        }
+    }
+
+    private fun previousFrameBonus(
+        lastFrame: FrameScores,
+        secondLastFrame: FrameScores,
+        currentFrame: FrameScores
+    ) = when {
+        (frameScores.size == 9) -> {
+            tenthFrameBonus(lastFrame, secondLastFrame, currentFrame)
+        }
+        (lastFrame.strike && !currentFrame.strike) -> {
+            frame.scoreWithBonus(
+                secondLastFrame.gameTotal, (lastFrame.first + currentFrame.frameTotal)
+            )
+        }
+        (lastFrame.spare) -> {
+            frame.scoreWithBonus(
+                secondLastFrame.gameTotal, (lastFrame.frameTotal + currentFrame.first)
+            )
+        }
+        else -> {
+            frame.score(lastFrame.first, lastFrame.second, secondLastFrame.gameTotal)
+        }
+    }
+
+    private fun multiStrikeSequenceBonus(
+        secondLastFrame: FrameScores,
+        lastFrame: FrameScores,
+        currentFrame: FrameScores
+    ) = when {
+        (secondLastFrame.strike && lastFrame.strike) -> {
+            frame.scoreWithBonus(secondLastFrame.gameTotal, (lastFrame.first + currentFrame.first))
+        }
+        else -> {
+            secondLastFrame.gameTotal
+        }
+    }
+
+    private fun firstFrameBonus(currentFrame: FrameScores) {
+        val lastFrame = frameScores[frameScores.size]
+        if (lastFrame != null) {
+            lastFrame.gameTotal = when {
+                (lastFrame.strike && !currentFrame.strike) -> {
+                    frame.scoreWithBonus(lastFrame.gameTotal, currentFrame.frameTotal)
+                }
+                (lastFrame.spare) -> {
+                    frame.scoreWithBonus(lastFrame.gameTotal, currentFrame.first)
+                }
+                else -> {
+                    lastFrame.gameTotal
+                }
+            }
+        }
+    }
+
+    private fun tenthFrameBonus(
+        lastFrame: FrameScores,
+        secondLastFrame: FrameScores,
+        currentFrame: FrameScores
+    ) = when {
+        (lastFrame.strike) -> {
+            frame.scoreWithBonus(
+                secondLastFrame.gameTotal, (lastFrame.first + currentFrame.frameTotal)
+            )
+        }
+        (lastFrame.spare) -> {
+            frame.scoreWithBonus(
+                secondLastFrame.gameTotal, (lastFrame.frameTotal + currentFrame.first)
+            )
+        }
+        else -> {
+            frame.score(lastFrame.first, lastFrame.second, secondLastFrame.gameTotal)
         }
     }
 }
